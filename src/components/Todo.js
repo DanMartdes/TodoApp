3b347a6d77ea630f3d3e1597e9a981add2bc1c8d
Daniel Martinez <danmartdesign@gmail.com>
2022-06-21 20:07:26 +0200

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,7 +4,6 @@ import Backdrop from "./Backdrop";
 
 function Todo(props) {
     const [modalIsOpen, setModalIsOpen] = useState(false);
-    const [deletedTodo, setDeletedTodo] = useState(false);
     function deleteHandler() {
         setModalIsOpen(true);
     }
@@ -13,22 +12,15 @@ function Todo(props) {
         setModalIsOpen(false);
     }
 
-    function deleteTodo() {
-        setDeletedTodo(true);
-    }
-
-
     return (
-        <div>
-            <div className='card'>
-                <h2>{props.text}</h2>
-                <div className='actions'>
-                    <button className='btn' onClick={deleteHandler}>Delete</button>
-                </div>
-                {/* { modalIsOpen ? <Modal /> : null } */}
-                {modalIsOpen && <Modal onCancel={closeModalHandler} onConfirm={deleteTodo} />}
-                {modalIsOpen && <Backdrop onClick={closeModalHandler} />}
+        <div className='card'>
+            <h2>{props.text}</h2>
+            <div className='actions'>
+                <button className='btn' onClick={deleteHandler}>Delete</button>
             </div>
+            {/* { modalIsOpen ? <Modal /> : null } */}
+            {modalIsOpen && <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />}
+            {modalIsOpen && <Backdrop onClick={closeModalHandler} />}
         </div>
     );
 }
